f4c2725460f3a55e2d005df5e333352b2131c404
stdatalabs <sachin.thirumala@gmail.com>
2016-10-09 00:51:40 -0700

    hive customInputFormat

---
+++ src/main/java/com/stdatalabs/hive/customInputFormat/customTextInputFormat.java
@@ -0,0 +1,31 @@
+package com.stdatalabs.hive.customInputFormat;
+
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileSplit;
+import org.apache.hadoop.mapred.InputSplit;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.RecordReader;
+import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.mapred.TextInputFormat;
+
+import com.stdatalabs.hive.customInputFormat.customRecordReader;;
+
+/**
+ * A custom input format for dealing with skipping records with specific length.
+ * 
+ * @author Sachin Thirumala
+ * 
+ */
+
+class CustomTextInputFormat extends TextInputFormat {
+
+	@Override
+	public RecordReader<LongWritable, Text> getRecordReader(InputSplit inputSplit, JobConf job, Reporter reporter)
+			throws IOException {
+		return new customRecordReader((FileSplit) inputSplit, job);
+	}
+}
